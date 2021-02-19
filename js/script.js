799ba42b86c0c5c32aec887ea95340621661ac03
mychae1l <69292242+mychae1l@users.noreply.github.com>
2021-02-19 18:47:35 +0400

    new site

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function(){
+
+    $("#quest1").click(function(){
+      $("#panel").slideToggle("slow");
+    });
+
+    $("#quest2").click(function(){
+        $("#panel2").slideToggle("slow");
+    });
+
+    $("#quest3").click(function(){
+        $("#panel3").slideToggle("slow");
+    });
+
+    $("#quest4").click(function(){
+        $("#panel4").slideToggle("slow");
+    });
+
+    $("#quest5").click(function(){
+        $("#panel5").slideToggle("slow");
+    });
+
+    $("#quest6").click(function(){
+        $("#panel6").slideToggle("slow");
+    });
+
+    $("#quest7").click(function(){
+        $("#panel7").slideToggle("slow");
+    });
+
+});
+
